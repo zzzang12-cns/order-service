a76c0c02612b6a290a9f2056ffe937ffbc735d13
zzzang12 <wadevs12@gmail.com>
2024-08-08 05:03:02 +0900

    Reactive servers with Spring WebFlux and Spring Data R2DBC

---
+++ src/main/java/com/polarbookshop/orderservice/order/domain/OrderService.java
@@ -0,0 +1,27 @@
+package com.polarbookshop.orderservice.order.domain;
+
+import org.springframework.stereotype.Service;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class OrderService {
+	private final OrderRepository orderRepository;
+
+	public OrderService(OrderRepository orderRepository) {
+		this.orderRepository = orderRepository;
+	}
+
+	public Flux<Order> getAllOrders() {
+		return orderRepository.findAll();
+	}
+
+	public Mono<Order> submitOrder(String isbn, int quantity) {
+		return Mono.just(buildRejectedOrder(isbn, quantity))
+		           .flatMap(orderRepository::save);
+	}
+
+	public static Order buildRejectedOrder(String bookIsbn, int quantity) {
+		return Order.of(bookIsbn, null, null, quantity, OrderStatus.REJECTED);
+	}
+}
