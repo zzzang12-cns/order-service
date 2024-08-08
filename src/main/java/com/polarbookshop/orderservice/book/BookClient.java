3fea7c69e96bd07325909ae09ffbf059dc2cf3fb
zzzang12 <wadevs12@gmail.com>
2024-08-09 00:51:06 +0900

    Resilient applications with Reactive Spring

--- src/main/java/com/polarbookshop/orderservice/book/BookClient.java
+++ src/main/java/com/polarbookshop/orderservice/book/BookClient.java
@@ -2,7 +2,11 @@ package com.polarbookshop.orderservice.book;
 
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
+
+import java.time.Duration;
 
 @Component
 public class BookClient {
@@ -16,6 +20,10 @@ public class BookClient {
 		return webClient.get()
 		                .uri("/books/{isbn}", isbn)
 		                .retrieve()
-		                .bodyToMono(Book.class);
+		                .bodyToMono(Book.class)
+		                .timeout(Duration.ofSeconds(3), Mono.empty())
+		                .onErrorResume(WebClientResponseException.NotFound.class, exception -> Mono.empty())
+		                .retryWhen(Retry.backoff(3, Duration.ofMillis(100)))
+		                .onErrorResume(Exception.class, exception -> Mono.empty());
 	}
 }
